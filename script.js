6554b1635f4a2e98358e858bfb2b82bac1611f23
Anirudh Ajith <anirudh.ajith@gmail.com>
2018-06-20 19:58:27 +0530

    Geolocation support

--- script.js
+++ script.js
@@ -2,19 +2,13 @@ var bangalore = { lat: 12.9716, lng: 77.5946 };
 var pos;
 var zoomLevel = 4;
 var map;
+var marker;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), { zoom: zoomLevel, center: bangalore, disableDoubleClickZoom: true, mapTypeControl: true });
     document.getElementsByName("zoom")[0].value = map.getZoom();
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            pos = {lat: position.coords.latitude, lng: position.coords.longitude};
-        });
-        var marker = new google.maps.Marker({ position: pos, map: map });
-        map.panTo(pos);
-    }
-
+    marker = new google.maps.Marker({ position: pos, map: map });
     map.addListener('zoom_changed', function() {
         console.log("Zoom Changed");
         document.getElementsByName("zoom")[0].value = map.getZoom();
@@ -29,3 +23,11 @@ function updateZoomLevel(newZL) {
     map.setZoom(parseInt(newZL));
 }
 
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+        map.panTo(pos);
+        marker.setPosition(pos);
+        map.setZoom(17);
+    });
+}
