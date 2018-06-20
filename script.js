0cd8860bb3a6e2cf42bf82c778260283e9e87deb
Anirudh Ajith <anirudh.ajith@gmail.com>
2018-06-20 19:42:09 +0530

    Double click recenters map, zoom-control fixed

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var bangalore = { lat: 12.9716, lng: 77.5946 };
+var pos;
 var zoomLevel = 4;
 var map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), { zoom: zoomLevel, center: bangalore });
+    map = new google.maps.Map(document.getElementById('map'), { zoom: zoomLevel, center: bangalore, disableDoubleClickZoom: true, mapTypeControl: true });
     document.getElementsByName("zoom")[0].value = map.getZoom();
-    var marker = new google.maps.Marker({ position: bangalore, map: map });
-    map.addListener('zoom_changed',function() {
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+        });
+        var marker = new google.maps.Marker({ position: pos, map: map });
+        map.panTo(pos);
+    }
+
+    map.addListener('zoom_changed', function() {
         console.log("Zoom Changed");
         document.getElementsByName("zoom")[0].value = map.getZoom();
+    });
+    map.addListener('dblclick', function(event){
+        console.log("Double Click");
+        map.panTo(event.latLng);
     });
 }
 
