b6040a7906497f2c423897fb992044972ef9c567
Anirudh Ajith <anirudh.ajith@gmail.com>
2018-06-21 17:04:19 +0530

    Markers

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var bangalore = { lat: 12.9716, lng: 77.5946 };
 var pos;
-var zoomLevel = 4;
+var zoomLevel = 9;
 var map;
 var name;
-var marker;
+var markers = {};
 
-function initMap() {
+function initMap2() {
     map = new google.maps.Map(document.getElementById('map'), { zoom: zoomLevel, center: bangalore, disableDoubleClickZoom: true, mapTypeControl: true });
     document.getElementsByName("zoom")[0].value = map.getZoom();
 
     marker = new google.maps.Marker({ position: pos, map: map });
     eventListeners();
-    if(document.cookie) {
+    if (document.cookie) {
         welcome();
     } else {
         name = prompt("Enter your name: ");
@@ -20,36 +20,74 @@ function initMap() {
     }
 }
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), { zoom: zoomLevel, center: bangalore, /*disableDoubleClickZoom: true,*/ mapTypeControl: true });
+    document.getElementsByName("zoom")[0].value = map.getZoom();
+
+    eventListeners();
+    performRequest();
+}
+
 function performRequest() {
     var invocation = new XMLHttpRequest();
     var url = 'https://iot.kpraveen.in/api/Users/5acb3b1a146ca8f84d18a8b2/data';
-    
-    if(invocation) {    
+
+    if (invocation) {
         invocation.open('GET', url, true);
-        invocation.onreadystatechange = function() {
+        invocation.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText);
+                updateMarkers(JSON.parse(this.responseText));
             }
         };
         invocation.send();
-        console.log(); 
+        console.log("Request successful");
+    } else {
+        console.log("Request failed");
     }
 }
 
+function updateMarkers(data) {
+    //console.log(data);
+    if (Object.keys(markers).length === 0 && markers.constructor === Object) {
+        for (person in data.information) {
+            console.log(data.information[person]);
+            pos = {
+                lat: data.information[person].latitude,
+                lng: data.information[person].longitude
+            };
 
-function updateZoomLevel(newZL) {
-    map.setZoom(parseInt(newZL));
+            markers[data.information[person].name] = new google.maps.Marker({ position: pos, map: map });
+            markers[data.information[person].name].addListener('click', function() {
+                map.panTo(this.getPosition());
+            });
+            //console.log(pos);
+        }
+    } else {
+        for (person in data.information) {
+            console.log(data.information[person]);
+            pos = {
+                lat: data.information[person].latitude,
+                lng: data.information[person].longitude
+            };
+
+            markers[data.information[person].name].setPosition(pos);
+            //console.log(pos);
+        }
+    }
+    console.log(markers);
 }
 
+
+
 function eventListeners() {
-    map.addListener('zoom_changed', function() {
+    map.addListener('zoom_changed', function () {
         console.log("Zoom Changed");
         document.getElementsByName("zoom")[0].value = map.getZoom();
     });
-    map.addListener('dblclick', function(event){
+    /* map.addListener('dblclick', function (event) {
         console.log("Double Click");
         map.panTo(event.latLng);
-    });
+    }); */
 }
 
 function welcome() {
@@ -60,7 +98,7 @@ function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
@@ -72,6 +110,14 @@ function getCookie(cname) {
     return "";
 }
 
+function updateZoomLevel(newZL) {
+    map.setZoom(parseInt(newZL));
+}
+
+setInterval(performRequest, 5000);
+
+
+/*
 if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         pos = {lat: position.coords.latitude, lng: position.coords.longitude};
@@ -83,3 +129,4 @@ if(navigator.geolocation) {
 
     });
 }
+*/
