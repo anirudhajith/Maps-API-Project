a5220e1be8e8bec0aa3584cca8f188fc1e24536b
Anirudh Ajith <anirudh.ajith@gmail.com>
2018-06-21 14:45:26 +0530

    Cross domain request

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var bangalore = { lat: 12.9716, lng: 77.5946 };
 var pos;
 var zoomLevel = 4;
 var map;
+var name;
 var marker;
 
 function initMap() {
@@ -9,6 +10,38 @@ function initMap() {
     document.getElementsByName("zoom")[0].value = map.getZoom();
 
     marker = new google.maps.Marker({ position: pos, map: map });
+    eventListeners();
+    if(document.cookie) {
+        welcome();
+    } else {
+        name = prompt("Enter your name: ");
+        document.cookie = "name=" + name;
+        welcome();
+    }
+}
+
+function performRequest() {
+    var invocation = new XMLHttpRequest();
+    var url = 'https://iot.kpraveen.in/api/Users/5acb3b1a146ca8f84d18a8b2/data';
+    
+    if(invocation) {    
+        invocation.open('GET', url, true);
+        invocation.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+            }
+        };
+        invocation.send();
+        console.log(); 
+    }
+}
+
+
+function updateZoomLevel(newZL) {
+    map.setZoom(parseInt(newZL));
+}
+
+function eventListeners() {
     map.addListener('zoom_changed', function() {
         console.log("Zoom Changed");
         document.getElementsByName("zoom")[0].value = map.getZoom();
@@ -19,8 +52,24 @@ function initMap() {
     });
 }
 
-function updateZoomLevel(newZL) {
-    map.setZoom(parseInt(newZL));
+function welcome() {
+    alert("Welcome " + getCookie("name") + "!");
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 if(navigator.geolocation) {
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+//Ok, write a small application that shows a Google map, in the browser, gets location of a few people from a http request (i will give you the api) and display them on the map, using NodeJS and host it on your domain.
+
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 var port = 5030;
 
@@ -6,7 +9,9 @@ console.log("Server running on localhost:" + port);
 console.log("Ctrl+C to stop...");
 
 http.createServer(function(req, res) {
-    fs.createReadStream("index.html").pipe(res);
+    var reqUrl = url.parse(req.url).pathname;
+    if(reqUrl == "/") reqUrl = "/index.html";
+    fs.createReadStream(reqUrl.slice(1)).pipe(res);
     res.writeHead(200);
-    console.log("**SERVED**");
+    console.log("**SERVED** " + reqUrl);
 }).listen(port);
