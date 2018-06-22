1362260e4b25befedaa67eb96f197066dd4935e5
Anirudh Ajith <anirudh.ajith@gmail.com>
2018-06-22 10:50:10 +0530

    Time ago and accuracy circle support

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var map;
 var name;
 var markers = {};
 var infoWindows = {};
+var circles = {};
 var follow;
 
 function initMap2() {
@@ -59,9 +60,29 @@ function updateMarkers(data) {
                 lng: data.information[person].longitude
             };
 
-            markers[data.information[person].name] = new google.maps.Marker({ position: pos, map: map, title: data.information[person].name, icon: 'http://iot.kpraveen.in/images/' + data.information[person].name + '.png', /*label: {color: "yellow", text: data.information[person].name}*/});
+            markers[data.information[person].name] = new google.maps.Marker({ 
+                position: pos, 
+                map: map, 
+                title: data.information[person].name, 
+                icon: 'http://iot.kpraveen.in/images/' + data.information[person].name + '.png',
+                /*label: {color: "yellow", text: data.information[person].name}*/
+            });
 
-            infoWindows[data.information[person].name] = new google.maps.InfoWindow({ content: data.information[person].name, title: data.information[person].name, buttons: { close: { visible: false } } });
+            infoWindows[data.information[person].name] = new google.maps.InfoWindow({ 
+                content: getTimeAgoString(data.information[person].lastLocationTime), 
+                title: data.information[person].name 
+            });
+
+            circles[data.information[person].name] = new google.maps.Circle({
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.35,
+                map: map,
+                center: pos,
+                radius: data.information[person].accuracy
+              });
 
             infoWindows[data.information[person].name].open(map, markers[data.information[person].name]);
 
@@ -85,6 +106,7 @@ function updateMarkers(data) {
             };
 
             markers[data.information[person].name].setPosition(pos);
+            infoWindows[data.information[person].name].setContent(getTimeAgoString(data.information[person].lastLocationTime));
             if (follow) map.panTo(follow.getPosition());
             //console.log(pos);
         }
@@ -92,7 +114,22 @@ function updateMarkers(data) {
     console.log(markers);
 }
 
-
+function getTimeAgoString(lastSeen) {
+    var now = new Date();
+    var ls = new Date(lastSeen);
+    var secAgo = Math.round((now - ls)/1000);
+    
+    if(secAgo < 60) { //seconds
+        return (secAgo + "s ago");
+    } else if(secAgo < 3600) { //minutes
+        return (Math.floor(secAgo/60) + "m ago");
+    } else if(secAgo < 86400) { //hours
+        return (Math.floor(secAgo/3600) + "h, " + Math.floor((secAgo%3600)/60) + "m ago");
+    } else {
+        return (Math.floor(secAgo/86400) + "d ago");
+    }
+    return ("Last seen on " + now);
+}
 
 function eventListeners() {
     map.addListener('zoom_changed', function () {
