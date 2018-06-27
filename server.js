90712778c1a04508d6445a186335b3552b5d68c9
Anirudh Ajith <anirudh.ajith@gmail.com>
2018-06-27 10:15:18 +0530

    Minor change to server implementation for major change in efficiency

--- server.js
+++ server.js
@@ -7,20 +7,23 @@ var request = require('request');
 var port = 5030;
 var apiKey;
 
-fs.readFile("api.key", "utf8", function (err, key) {
-    if (err) throw err;
-    apiKey = key;
-    var data = getData();
-    setInterval(function() {data = getData();}, 5000);
-});
+
 
 
 console.log("Server running on localhost:" + port);
 console.log("Ctrl+C to stop...");
 
-http.createServer(function(req, res) {
+http.createServer(function (req, res) {
+
+    fs.readFile("api.key", "utf8", function (err, key) {
+        if (err) throw err;
+        apiKey = key;
+        var data = getData();
+        setInterval(function () { data = getData(); }, 5000);
+    });
+
     var reqUrl = url.parse(req.url).pathname;
-    if(reqUrl == "/") reqUrl = "/index.html";
+    if (reqUrl == "/") reqUrl = "/index.html";
     fs.createReadStream(reqUrl.slice(1)).pipe(res);
     res.writeHead(200);
     console.log("**SERVED** " + reqUrl);
@@ -28,19 +31,19 @@ http.createServer(function(req, res) {
 
 
 function getData() {
-   request({
-      uri: "https://iot.kpraveen.in/api/Users/5acb3b1a146ca8f84d18a8b2/data?access_token=" + apiKey,
-      method: "GET",
-      timeout: 10000,
-      followRedirect: true,
-      maxRedirects: 10
-   }, function(error, response, body) {
-     console.log(body);
-     fs.writeFile("data", body, function(err) {
-        if(err) {
-           return console.log(err);
-        }
-     }); 
-   }); 
+    request({
+        uri: "https://iot.kpraveen.in/api/Users/5acb3b1a146ca8f84d18a8b2/data?access_token=" + apiKey,
+        method: "GET",
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10
+    }, function (error, response, body) {
+        console.log(body);
+        fs.writeFile("data", body, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    });
 }
 
